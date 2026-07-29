8e3d723dc2ada27abcb045f56f54ab22cc0a3f94
최성헌 <sunsetrome@korail.com>
2026-07-29 12:54:09 +0900

    fix(android): enable fullscreen sermon video

--- chflow-expo/App.tsx
+++ chflow-expo/App.tsx
@@ -450,6 +450,7 @@ function AppWebView() {
           javaScriptEnabled={true}
           // 파일 업로드 허용 (프로필 사진 등)
           allowsInlineMediaPlayback={true}
+          allowsFullscreenVideo={true}
           mediaPlaybackRequiresUserAction={false}
           // 줌 비활성 (모바일 웹에서 의도치 않은 확대 방지)
           scalesPageToFit={false}
